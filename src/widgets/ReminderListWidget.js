1c53be26da4129674a5b0b622a38fde44f781edd
Billrawknow <bildadronoh@gmail.com>
2023-07-30 21:46:15 +0300

    button

--- src/widgets/ReminderListWidget.js
+++ src/widgets/ReminderListWidget.js
@@ -73,6 +73,7 @@ export default function ReminderListWidget() {
                   reminderList.splice(index, 1)
                   setReminderListData(reminderList)
                 }}
+                
               >X</button>
               
             </div>
